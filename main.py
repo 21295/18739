636ab68dd3f513b8358082615bcf207d1848cbaf
21295 <131825832+21295@users.noreply.github.com>
2023-06-26 03:07:49 +0000

    did sequence 2, asked if they wanted to see the menu

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+#2
 # welcome user to canteen
 # ask for name
 # welcome the user by name
-print("Hello!,")
-print("Welcome to our canteen!")
+print("Kia Ora!,")
+print("Welcome to our school canteen!")
 Name = input("What is your name? ")
-if Name.lower():
-  welcome = "Hello and welcome {name}, Welcome to our canteen!"
-print(welcome.format(name=Name))
+if Name.isalpha():
+  welcome = "Kia Ora, {name} welcome to the Fraser High School canteen."
+  print(welcome.format(name=Name))
 
-while Name.lower() == False:
-  print("Sorry we need your name for your order!")
+while Name.isalpha() == False:
+  print("Sorry we really need to know your name to help us process this order")
   Name = input ("What is your name? ")
+
+# s2
+# ask user if they want to see the menu, yes or no
+# if no thank user for visiting
+
+menuQuestion = "{name} , would you like to see our awesome menu?  Please enter Yes or No."
+menuResponse = input (menuQuestion.format(name=Name))
+
+while menuResponse != "Yes" and menuResponse != "No":
+  print("Sorry {name} , we need a Yes or No answer to help us.")
+
+if menuResponse == "No":
+  print("Thank you {name} for visiting us at Fraser High School Canteen.".format(name=Name))
+
+# show menu to user
