336c19a899641da60e3712dabd9a7bf3186e1f7c
21295 <131825832+21295@users.noreply.github.com>
2023-07-17 02:53:55 +0000

    all edits and changes (forgot to stage and commit code last time)

--- main.py
+++ main.py
@@ -4,26 +4,54 @@
 # welcome the user by name
 print("Kia Ora!,")
 print("Welcome to our school canteen!")
-Name = input("What is your name? ")
-if Name.isalpha():
-  welcome = "Kia Ora, {name} welcome to the Fraser High School canteen."
-  print(welcome.format(name=Name))
-
-while Name.isalpha() == False:
-  print("Sorry we really need to know your name to help us process this order")
-  Name = input ("What is your name? ")
+namesetup = True
+while namesetup == True:
+  global name
+  name = input("What is your name? ")
+  if name.isalpha():
+    welcome = "Kia Ora, {} welcome to the Fraser High School canteen."
+    print(welcome.format(name))
+    namesetup = False
+  elif name.isalpha() == False:
+    name = input ("What is your name? ")
+    print("Sorry we really need to know your name to help us process this order")
 
 # s2
 # ask user if they want to see the menu, yes or no
 # if no thank user for visiting
 
-menuQuestion = "{name} , would you like to see our awesome menu?  Please enter Yes or No."
-menuResponse = input (menuQuestion.format(name=Name))
 
-while menuResponse != "Yes" and menuResponse != "No":
-  print("Sorry {name} , we need a Yes or No answer to help us.")
+menuResponse1 = True
+foodRespons1e1 = True
+yes = ["yes","Yes","y","Y"]
+no = ["no","No","n","N"]
+while menuResponse1 == True:
+  print( f"{name} , would you like to see our awesome menu? Please enter Yes or No.")
+  menuQuestion = input()
+  if menuQuestion in yes:
+    menuResponse1 = False
+  elif menuQuestion in no:
+    print(f"Thank you {name} for visiting us at Fraser High School Canteen.")
+    exit()
+  else:
+    print("Sorry {} we need a Yes or No answer to help us.".format(name))
 
-if menuResponse == "No":
-  print("Thank you {name} for visiting us at Fraser High School Canteen.".format(name=Name))
-
-# show menu to user
+# s3
+# taking the users order
+lastthing = True
+finalResponse = "AE"
+while lastthing == True:
+  menuList = input(f"The Pie is worth $4.50 and The Burger is worth $7.89, {name} what would you like to order, either a Pie or Burger?  (Use lowercase) ")
+  foodResponse = (menuList.format(name))
+  if foodResponse == "pie":
+    meal="pie"
+    foodRespons1e1 = False
+    lastthing = False
+  elif foodResponse == "burger":
+    meal="burger"
+    foodRespons1e1 = False
+    lastthing = False
+  else:
+    print(f"Sorry {name} we need to know if you would like a 'Pie' or a 'Burger'?".format(name))
+finalResponse = f"Thank you {name} for ordering the delicious {meal}. We will just get this ready for you."
+print(finalResponse.format(name=name))
